9031ca68b10971656558125c4a692002158b0fe9
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2023-08-24 07:08:13 +0800

    feat: update constants

--- plugin/src/constants.ts
+++ plugin/src/constants.ts
@@ -1,11 +1,11 @@
 export const NODE_TYPES = {
-  Post: `Post`,
-  Author: `Author`,
-  Asset: `Asset`
+  Post: "Post",
+  Author: "Author",
+  Asset: "Asset",
 } as const;
 
 export const CACHE_KEYS = {
-  Timestamp: `timestamp`
+  Timestamp: "timestamp",
 } as const;
 
 /**
@@ -13,5 +13,33 @@ export const CACHE_KEYS = {
  * For example, you could start all third-party API errors with 1000x, all transformation errors with 2000x, etc.
  */
 export const ERROR_CODES = {
-  GraphQLSourcing: `10000`
+  GraphQLSourcing: "10000",
 } as const;
+
+// Environment
+export const IS_DEV = process.env.NODE_ENV === "development";
+export const IS_PROD = process.env.NODE_ENV === "production";
+
+// App
+export const APP_NAME = "@stackmarketlabs/gatsby-source-bigcommerce";
+
+// Request
+export const REQUEST_BIGCOMMERCE_API_URL = "https://api.bigcommerce.com";
+export const REQUEST_ACCEPT_HEADER = "application/json";
+export const REQUEST_TIMEOUT = 60000;
+export const REQUEST_THROTTLE_INTERVAL = 500;
+export const REQUEST_DEBOUNCE_INTERVAL = 500;
+export const REQUEST_CONCURRENCY = 100;
+export const REQUEST_PENDING_COUNT = 0;
+export const REQUEST_RESPONSE_TYPE = "json";
+
+// Headers
+export const ACCESS_CONTROL_ALLOW_HEADERS = "Content-Type,Accept";
+export const ACCESS_CONTROL_ALLOW_CREDENTIALS = true;
+export const CORS_ORIGIN = "*";
+
+// Hooks
+export const BIGCOMMERCE_WEBHOOK_API_ENDPOINT = "/v3/hooks";
+
+// Auth
+export const AUTH_HEADERS = {} as const;
